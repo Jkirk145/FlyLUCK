6c87b418e4c3a75747d6f1fc2c9a9d98633d0a19
john.kirksey@verizon.net <eMbr@erPhenom300>
2016-12-30 14:22:28 -0500

    Created CalendarView and add to CalendarPage.

--- FlyLUCK/CalendarPage.xaml.cs
+++ FlyLUCK/CalendarPage.xaml.cs
@@ -2,71 +2,18 @@
 using System.Collections.Generic;
 
 using Xamarin.Forms;
-using XLabs.Forms.Controls;
 
 namespace FlyLUCK
 {
 	public partial class CalendarPage : ContentPage
 	{
-		CalendarView _calendarView;
-		StackLayout _stacker;
-		Label _flightinfo;
-		Button _closepage;
-		Button _SelectDate;
-
-		void ClosePage(Object sender, EventArgs e)
-		{
-			this.Navigation.PopModalAsync();
-		}
-
-		void SelectDate(Object sender, EventArgs e)
-		{
-			_calendarView.SelectedDate = new DateTime(2016, 11, 8);
-
-			_calendarView.SelectedDate = new DateTime(2016, 11, 26);
-		}
-
-
+		private CalendarView _cal;
 		public CalendarPage()
 		{
 			InitializeComponent();
-
-			Title = "Aircraft Calendar";
-			_stacker = new StackLayout();
-			_flightinfo = new Label
-			{
-				VerticalOptions = LayoutOptions.Start,
-				HorizontalOptions = LayoutOptions.CenterAndExpand
-			};
-			_closepage = new Button
-			{
-				Image = "close.png"
-			};
-			_closepage.Clicked += ClosePage;
-
-			_SelectDate = new Button { Text = "Do It" };
-			_SelectDate.Clicked += SelectDate;
-
-			_stacker.Padding = 30;
-			Content = _stacker;
-
-			_calendarView = new CalendarView
-			{
-				VerticalOptions = LayoutOptions.Center,
-				HorizontalOptions = LayoutOptions.Center,
-				MinDate = CalendarView.FirstDayOfMonth(DateTime.Now),
-				MaxDate = CalendarView.LastDayOfMonth(DateTime.Now.AddYears(1))
-			};
-			_stacker.Children.Add(_closepage);
-			_stacker.Children.Add(_calendarView);
-			_stacker.Children.Add(_flightinfo);
-			_stacker.Children.Add(_SelectDate);
-
-			_calendarView.DateSelected += (object sender, DateTime e) =>
-			{
-				_flightinfo.Text = "Date Was Selected" + e.ToString("d");
-			};
-
+			_cal = new CalendarView();
+			sLayout.Children.Add(_cal);
+			       
 		}
 	}
 }
---
+++ FlyLUCK/CalendarView.xaml.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using Xamarin.Forms;
+
+namespace FlyLUCK
+{
+	public partial class CalendarView : ContentView
+	{
+
+		private int _colCount = 0;
+		private DateTime _today;
+		private DateTime _calDate;
+
+		public CalendarView()
+		{
+			InitializeComponent();
+
+			_today = DateTime.Now;
+			_calDate = _today;
+
+			cmdNext.Clicked += Next_Month;
+			cmdPrev.Clicked += Prev_Month;
+			cmdPrev.IsVisible = false;
+			BuildGrid(_today);
+		}
+
+
+		void BuildGrid(DateTime today)
+		{
+			MonthName.Text = today.ToString("MMM"); ;
+			MonthGrid.Children.Add(new Label { HorizontalOptions = LayoutOptions.End, Text = "Su", BackgroundColor = Color.Silver, TextColor = Color.Black }, 0, 0);
+			MonthGrid.Children.Add(new Label { HorizontalOptions = LayoutOptions.End, Text = "Mo", BackgroundColor = Color.Silver, TextColor = Color.Black }, 1, 0);
+			MonthGrid.Children.Add(new Label { HorizontalOptions = LayoutOptions.End, Text = "Tu", BackgroundColor = Color.Silver, TextColor = Color.Black }, 2, 0);
+			MonthGrid.Children.Add(new Label { HorizontalOptions = LayoutOptions.End, Text = "We", BackgroundColor = Color.Silver, TextColor = Color.Black }, 3, 0);
+			MonthGrid.Children.Add(new Label { HorizontalOptions = LayoutOptions.End, Text = "Th", BackgroundColor = Color.Silver, TextColor = Color.Black }, 4, 0);
+			MonthGrid.Children.Add(new Label { HorizontalOptions = LayoutOptions.End, Text = "Fr", BackgroundColor = Color.Silver, TextColor = Color.Black }, 5, 0);
+			MonthGrid.Children.Add(new Label { HorizontalOptions = LayoutOptions.End, Text = "Sa", BackgroundColor = Color.Silver, TextColor = Color.Black }, 6, 0);
+
+			int rowPos = 1;
+			int colPos = 0;
+
+			DateTime firstDayOfMonth = new DateTime(today.Year, today.Month, 1);
+			colPos = (int)firstDayOfMonth.DayOfWeek;
+
+			MonthGrid.Children.Add(new Label { HorizontalTextAlignment = TextAlignment.End, Text = "1", BackgroundColor = Color.Silver, TextColor = Color.White }, colPos, rowPos);
+			int numDays = DateTime.DaysInMonth(today.Year, today.Month);
+
+			for (int i = 2; i <= numDays; i++)
+			{
+				colPos += 1;
+				if (colPos == 7)
+				{
+					colPos = 0;
+					rowPos += 1;
+				}
+				MonthGrid.Children.Add(new Label { HorizontalTextAlignment = TextAlignment.End, Text = i.ToString(), BackgroundColor = Color.Silver, TextColor = Color.White }, colPos, rowPos);
+
+			}
+		}
+
+		void Next_Month(object sender, EventArgs e)
+		{
+			_calDate = _calDate.AddMonths(1);
+			MonthGrid.Children.Clear();
+			BuildGrid(_calDate);
+			cmdPrev.IsVisible = true;
+		}
+
+		void Prev_Month(object sender, EventArgs e)
+		{
+			
+			_calDate = _calDate.AddMonths(-1);
+			if (_calDate.Month == _today.Month)
+			{
+				cmdPrev.IsVisible = false;
+			}
+			MonthGrid.Children.Clear();
+			BuildGrid(_calDate);
+		}
+
+	}
+}
